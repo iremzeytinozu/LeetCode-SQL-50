ebb2998ab7988a96df8734c2f0904577b47da158
iremzeytinozu <iremzeytinozu@gmail.com>
2025-03-04 10:15:31 +0300

    Create Triangle Judgement.sql

---
+++ Triangle Judgement.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    *,
+    CASE
+        WHEN (x + y > z AND y + z > x AND x + z > y) THEN 'Yes'
+        ELSE 'No'
+    END AS Triangle
+FROM Triangle
